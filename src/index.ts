a51dee2a68a6063fd223733826f6c91dd8d017bb
solufa <solufa2020@gmail.com>
2022-09-11 21:23:18 +0900

    chore: add allow-run option

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,23 @@ import { sleep } from "sleep";
 
 console.log("Welcome to Deno!");
 
+const p = Deno.run({
+  cmd: ["echo", "hello"],
+  stdout: "piped",
+  stderr: "piped",
+});
+
+const { code } = await p.status();
+
+if (code === 0) {
+  const rawOutput = await p.output();
+  await Deno.stdout.write(rawOutput);
+} else {
+  const rawError = await p.stderrOutput();
+  const errorString = new TextDecoder().decode(rawError);
+  console.log(errorString);
+}
+
 for await (const _ of readLines(Deno.stdin)) {
   console.log(_);
   await sleep(3);
